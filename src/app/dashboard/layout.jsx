6ed5bc4baf718307a8361b7becb90e3614d5d1a6
zThundy <antonioantonelli7@gmail.com>
2024-05-31 15:58:09 +0200

    moved header to page

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,20 +1,11 @@
-import { Container, Checkbox, FormControlLabel } from "@mui/material";
 
 import style from "./layout.module.css";
+import React from "react";
 
 export default function DashboardLayout({ children }) {
+
   return (
     <div className={style.container}>
-      <div className={style.header}>
-        <div className={style.selectAllContainer}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" />}
-            label="Select all"
-          />
-        </div>
-
-        
-      </div>
       {children}
     </div>
   );

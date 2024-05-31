da41a00c61bd8cae68eda49636153df66fd7657e
zThundy <antonioantonelli7@gmail.com>
2024-05-31 15:58:01 +0200

    Added images config nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// configure image optimization
+nextConfig.images = {
+  remotePatterns: [
+    {
+      // match any image URL on a domain
+      hostname: "via.placeholder.com",
+    },
+    {
+      // match any image URL on a domain
+      hostname: "picsum.photos",
+    },
+  ],
+  deviceSizes: [320, 420, 768, 1024, 1200],
+  imageSizes: [16, 32, 48, 64, 96],
+  path: "/_next/image",
+  loader: "default",
+};
+
 export default nextConfig;

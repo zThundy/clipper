1c9c1ba7efa5133a29f4480191318c994d7e7302
zThundy <antonioantonelli7@gmail.com>
2024-06-06 19:09:08 +0200

    styling of not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,32 @@
+import { Container } from '@mui/material';
+
+import style from './not-found.module.css';
+
 export default function NotFound() {
   return (
-    <main>
-      not found daje roma
-    </main>
+    <Container className={style.secondaryContainer}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "auto",
+          textAlign: "center",
+          userSelect: "none",
+        }}
+      >
+        <h1 className={style.textTwitch}>Sorry </h1>
+        <h2 style={{
+          fontSize: "3.5rem",
+          margin: "0",
+        }}>😢</h2>
+        </div>
+      <div style={{
+        color: "var(--mui-palette-text-primary)",
+      }}>
+        We couldn't find the page you were looking for.
+      </div>
+    </Container>
   );
 }

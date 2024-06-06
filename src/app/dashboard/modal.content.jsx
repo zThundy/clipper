86dbf0181f1ad8b6a33a35f1c14f8d3924a8cb14
zThundy <antonioantonelli7@gmail.com>
2024-06-06 19:10:15 +0200

    minor changes to modal in dashboard

--- src/app/dashboard/modal.content.jsx
+++ src/app/dashboard/modal.content.jsx
@@ -1,3 +1,5 @@
+import { Divider } from "@mui/material";
+
 import modal from "./page.modal.module.css";
 
 export default function ModalContent({ clip }) {
@@ -5,8 +7,14 @@ export default function ModalContent({ clip }) {
     <div className={modal.modalStuffBg}>
       <div className={modal.modalStuff}>
         <h2>{clip.title}</h2>
+        <Divider
+          style={{
+            backgroundColor: "var(--mui-palette-background-paper)"
+          }}
+        />
         <p>{clip.description}</p>
-        <img src={clip.image} alt={clip.title} />
+        {/* <img src={clip.thumbnail_url.replace("%{width}", 240).replace("%{height}", 110)} alt={clip.title} /> */}
+        <video src={clip.url} controls width="100%" />
         <a href={clip.url} target="_blank" rel="noreferrer">Watch</a>
       </div>
     </div>

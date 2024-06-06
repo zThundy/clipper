a29e5b557fd410378442e2f6d7fecef6b4fe63c7
zThundy <antonioantonelli7@gmail.com>
2024-06-06 19:09:44 +0200

    added metadata in dashboard

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,6 +2,11 @@
 import style from "./layout.module.css";
 import React from "react";
 
+export const metadata = {
+  title: "Clipper - Dashboard",
+  description: "The best way to manage your twitch clips",
+};
+
 export default function DashboardLayout({ children }) {
 
   return (
